a92b844b97d75410a68df2eaa32bb5772a319e00
msyed2020 <mikailsyed@yahoo.com>
2025-02-28 16:56:34 -0500

    printvar

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,7 @@ const fs = require('fs'); // fs stands for filesystem
 
 const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
+console.log(textInput);
 
 //const greet = "Hello";
 //console.log(greet);
