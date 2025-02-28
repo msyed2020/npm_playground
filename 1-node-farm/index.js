2ea4a83b8e518315aca6016f149e5ee8d15913b2
msyed2020 <mikailsyed@yahoo.com>
2025-02-28 17:16:53 -0500

    Async print

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,19 +2,19 @@ const fs = require('fs'); // fs stands for filesystem
 
 // sync way
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textInput);
+// console.log(textInput);
 
-const textOutput = `What we got: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOutput);
-console.log("Wrote file");
+// const textOutput = `What we got: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOutput);
+// console.log("Wrote file");
 
 //const greet = "Hello";
 //console.log(greet);
 
 // async way
 
-fs.readFile('./txt/start.txt', (err, data) => {
-
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    console.log(data);
 });
