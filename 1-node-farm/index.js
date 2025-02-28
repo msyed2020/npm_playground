a9cef7e3f8d1c483521b8d6601a16a64d92fb260
msyed2020 <mikailsyed@yahoo.com>
2025-02-28 17:19:07 -0500

    Uncomment

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,13 +2,13 @@ const fs = require('fs'); // fs stands for filesystem
 
 // sync way
 
-// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-// console.log(textInput);
+console.log(textInput);
 
-// const textOutput = `What we got: ${textInput}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOutput);
-// console.log("Wrote file");
+const textOutput = `What we got: ${textInput}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOutput);
+console.log("Wrote file sync");
 
 //const greet = "Hello";
 //console.log(greet);
