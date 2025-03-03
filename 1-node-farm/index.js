6279357b6538341ad9d6af548c80b726b764bf8d
msyed2020 <mikailsyed@yahoo.com>
2025-03-03 12:21:34 -0500

    Server setup

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -33,6 +33,10 @@ const http = require('http'); // build webserver, include http
 
 // Server stuff (actually needed lol)
 
-http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.end("Server");
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
 });
