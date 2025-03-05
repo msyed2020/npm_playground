897a9b7d33a3b4c527e920f05e85f6039f5fd2b2
msyed2020 <mikailsyed@yahoo.com>
2025-03-04 19:40:46 -0500

    Delete fileread

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,34 +3,6 @@ const http = require('http'); // build webserver, include http
 
 // File stuff (demo only, no longer needed, hence commented)
 
-// sync way
-
-// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-
-// console.log(textInput);
-
-// const textOutput = `What we got: ${textInput}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOutput);
-// console.log("Wrote file sync");
-// console.log("\n=========================\n");
-//const greet = "Hello";
-//console.log(greet);
-//const ability = "Welcome to Node.js!! I guess XD"
-//console.log(ability);
-
-// async way
-
-// fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-//     console.log(data);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
-//         console.log(data2);
-        
-//         fs.writeFile('./txt/final.txt', `${data}\n${data2}`, 'utf-8', err => {
-//             console.log("File has been written");
-//         });
-//     });
-// });
-
 // Server stuff (actually needed lol)
 
 const server = http.createServer((req, res) => {
