ca782bc60ed467c9d969c672411be05131fa449d
msyed2020 <mikailsyed@yahoo.com>
2025-03-12 15:08:13 -0400

    Bruh too many things

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,8 +3,13 @@ const http = require('http'); // build webserver, include http
 const url = require('url'); // Needed for routing
 
 const replaceTemplate = (temp, prod) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, prod.productName); // //g means apply globally to all variables
 
+    // Just so we're clear, since this is proof of concept and not a real functioning site,
+    // only a few parameters have been changed from the given code. For instance,
+    // image has not been edited so all items have cheese emoji
+
+    let output = temp.replace(/{%PRODUCTNAME%}/g, prod.productName); // //g means apply globally to all variables
+    output = temp.replace(/{%PRICE%}/g, prod.price);
     if (!prod.organic) {
         output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     }
@@ -26,10 +31,10 @@ const server = http.createServer((req, res) => { // req url is page url, templat
         res.writeHead(200, { 'Content-type': 'text/html' });
 
         const cards = prodData.map(el => replaceTemplate(card, el)).join('');
-
+        const output = overview.replace('{%PRODUCT_CARD%}', cards);
         //console.log(cards);
 
-        res.end(overview);
+        res.end(output);
     }
     // Projects
     else if (path === '/projects') {
