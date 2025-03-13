921a1272e93e8597c93709fe3a0bccd40d7b1777
msyed2020 <mikailsyed@yahoo.com>
2025-03-13 16:39:49 -0400

    Edit replace template

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -10,6 +10,7 @@ const replaceTemplate = (temp, prod) => {
 
     let output = temp.replace(/{%PRODUCTNAME%}/g, prod.productName); // //g means apply globally to all variables
     output = output.replace(/{%PRICE%}/g, prod.price);
+    output = output.replace(/{%LOCATION%}/g, prod.from);
     if (!prod.organic) {
         output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     }
