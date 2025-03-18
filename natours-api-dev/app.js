115245b659294be890ff026e1a4e68be707e3d5a
msyed2020 <mikailsyed@yahoo.com>
2025-03-17 20:05:17 -0400

    POST test

--- natours-api-dev/app.js
+++ natours-api-dev/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.status(200).send('Server stuff');
 });
 
+app.post('/', (req, res) => {
+    res.send("POST");
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
