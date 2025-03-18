8366ff40530e3ee69bec33f8f15f1a92f5d505ca
msyed2020 <mikailsyed@yahoo.com>
2025-03-17 20:01:54 -0400

    Server test

--- natours-api-dev/app.js
+++ natours-api-dev/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Server stuff');
+});
+
 const port = 3000;
-app.listen(port);
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
