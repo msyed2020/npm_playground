bc4c69a99411b194e440ab35eb56ab22e487d021
msyed2020 <mikailsyed@yahoo.com>
2025-03-18 21:35:26 -0400

    GET handling

--- natours-api-dev/app.js
+++ natours-api-dev/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Server stuff');
-});
+// app.get('/', (req, res) => { // Basic GET stuff
+//     res.status(200).send('Server stuff');
+// });
 
-app.post('/', (req, res) => {
-    res.send("POST");
-})
+// app.post('/', (req, res) => {
+//     res.send("POST");
+// })
 
 const port = 3000; // need to change port # later
 app.listen(port, () => {
