bbf7020b54887af6c7d35f987c7fd3bcc716a397
msyed2020 <mikailsyed@yahoo.com>
2025-04-08 10:52:42 -0400

    j

--- mongodb_journal/journal.js
+++ mongodb_journal/journal.js
@@ -1,3 +1,4 @@
 console.log("4-7-2025: Got Compass installed.");
 console.log("4-7-2025: Finally connected to Compass lol");
 console.log("4-7-2025: Basic MongoDB working, will get to Mongoose tmrw");
+console.log("4-8-2025: Connected to database using VSCode");
